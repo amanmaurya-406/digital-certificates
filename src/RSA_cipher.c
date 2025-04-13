892079bc158a0acad7644df18c76ddd05462ede4
amanmaurya-406 <amanmaurya8661@gmail.com>
2025-04-14 03:51:52 +0530

    changed code to pre-initialize parameters

--- src/RSA_cipher.c
+++ src/RSA_cipher.c
@@ -1,11 +1,9 @@
 #include "RSA_cipher.h"
 
 void RSA_private_encrypt(mpz_t signature, mpz_t hash, mpz_t priv_exp, mpz_t mod){  
-    mpz_init(signature);
     mpz_powm(signature, hash, priv_exp, mod);   
 }
 
 void RSA_public_decrypt(mpz_t hash, mpz_t signature, mpz_t pub_key, mpz_t mod){
-    mpz_init(hash);
     mpz_powm(hash, signature, pub_key, mod);
 }
