a292ad2b21cc63f9b5c81e8500f1fb0dd80393a4
amanmaurya-406 <amanmaurya8661@gmail.com>
2025-04-14 03:49:43 +0530

    upgrade function

--- src/generate_CA.c
+++ src/generate_CA.c
@@ -2,19 +2,20 @@
 #include "RSA_generateKey.h"
 #include "encodeKey.h"
 #include "generateCertificate.h"
+#include "utils.h"
 
-Certificate *generateCA(Info caInfo){
+Certificate *generateCA(DName *caInfo){
 
-    if(generate_RSA_keys(DATA_DIR "/ca_private.key"))
+    if(generate_RSA_key(DATA_DIR "/ca_private.key"))
         printf("Key generated and stored as '%s'\n", "ca_private.key");
     else{
         printf("Key generation failed\n");
         return NULL;
     }
 
-    PrivateKey *ca_privKey = load_privateKey(DATA_DIR "/ca_private.key");
+    PrivateKey *ca_privKey = d2i_RSAPrivateKey(DATA_DIR "/ca_private.key");
     Certificate *ca_cert = generate_self_signed_certificate(caInfo, ca_privKey);
-
+    
     free_privateKey(ca_privKey);
     return ca_cert;
 }
