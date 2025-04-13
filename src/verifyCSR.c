8829b5146dfbf06974f160c711b7dba0d850634d
amanmaurya-406 <amanmaurya8661@gmail.com>
2025-04-14 03:54:12 +0530

    changed function syntax

--- src/verifyCSR.c
+++ src/verifyCSR.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
+#include <error.h>
 #include "serialize.h"
 #include "sha512.h"
 #include "RSA_cipher.h"
 #include "verifyCSR.h"
 
+#define SHA512_DIGEST_LENGTH 64
 
-// 1. Signature Verification
-bool verify_CSRSignature(CSR csr){
+// Signature Verification
+bool verify_CSRSignature(CSR *csr){
 
-    size_t csrDataLen;
-    uint8_t *csrData = serialize_CSR(csr, &csrDataLen);
-    uint8_t *hash = SHA512(csrData, csrDataLen);
+    size_t csrInfo_size;
+    uint8_t *csrInfo_s = serialize_CSRInfo(csr->csrInfo, &csrInfo_size);
+    uint8_t *csrInfoHash = SHA512(csrInfo_s, csrInfo_size);
     
     mpz_t computed_signature, received_signature;
-    mpz_init(computed_signature);
-    mpz_import(computed_signature, SHA512_DIGEST_LENGTH, 1, 1, 0, 0, hash);
+    mpz_inits(computed_signature, received_signature, NULL);
+    mpz_import(computed_signature, SHA512_DIGEST_LENGTH, 1, 1, 0, 0, csrInfoHash);
     
-    RSA_public_decrypt(received_signature, csr.signature->value, csr.publicKey->exponent, csr.publicKey->modulus);
+    RSA_public_decrypt(received_signature, csr->signature, csr->csrInfo->pKeyInfo->pubKey->e, csr->csrInfo->pKeyInfo->pubKey->n);
     bool comparison_result = !mpz_cmp(computed_signature, received_signature);
     
     mpz_clears(computed_signature, received_signature, NULL);
@@ -24,23 +26,23 @@ bool verify_CSRSignature(CSR csr){
     return comparison_result;
 }
 
-// 2. Public Key Verification
-bool verify_rsaPublicKey(PublicKey *publicKey){
-    if(mpz_sizeinbase(publicKey->modulus, 256) < 2048 / 8){ // 2048 bits
-        printf("Modulus size is too small\n");
+// Public Key Verification
+bool verify_RSAPublicKey(PublicKey *publicKey){
+    if(mpz_sizeinbase(publicKey->n, 256) < 2048 / 8){ // 2048 bits
+        printf("RSA key size is too small, choose atleast 2048 bits\n");
+        // errno = ERROR_BAD_LENGTH;
         return 0;
     }
 
-    if(!(mpz_sizeinbase(publicKey->exponent, 256) == 3 && !mpz_cmp_ui(publicKey->exponent, 65537)) && \
-       !(mpz_sizeinbase(publicKey->exponent, 256) == 1 && !mpz_cmp_ui(publicKey->exponent, 3))){
+    if(!(mpz_sizeinbase(publicKey->e, 256) == 3 && !mpz_cmp_ui(publicKey->e, 65537)) && \
+       !(mpz_sizeinbase(publicKey->e, 256) == 1 && !mpz_cmp_ui(publicKey->e, 3))){
             printf("Unusual exponent\n");
             return 0;
     }
 
-    // Public Key Valid.
     return 1;
 }
 
-// 3. Subject Name Validation
+// Subject Name Validation
 // bool verifyCSRSubjectInfo(Info sub_Info){ }
 
