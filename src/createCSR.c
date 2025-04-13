9db52aa0d1312cf7bdaef5b2534b70f1ee170169
amanmaurya-406 <amanmaurya8661@gmail.com>
2025-04-14 03:48:03 +0530

    Refactor: modularize code structure for better readability and maintainbility

--- src/createCSR.c
+++ src/createCSR.c
@@ -1,72 +1,90 @@
 #include "common.h"
+#include "utils.h"
+#include "RSA_generateKey.h"
 #include "serialize.h"
-#include "encodeKey.h"
 #include "sha512.h"
 #include "RSA_cipher.h"
 #include "cer2txt.h"
 
+static void get_csrInfo(CSRInfo *csrInfo, DName *subject, PrivateKey *privKey){
+    csrInfo->version = 0x00;  // v1
+    copy_dName(csrInfo->subject, subject);
+    
+    csrInfo->pKeyInfo->pubKeyAlgo = malloc(strlen("rsaEncryption") + 1);
+    strcpy(csrInfo->pKeyInfo->pubKeyAlgo, "rsaEncryption");
+    
+    extract_publicBytes(csrInfo->pKeyInfo->pubKey, privKey);
+}
 
-CSR *createCSR(PrivateKey *privateKey, Info subject){
+static uint8_t *serializeAndSignCSR(size_t *output_size, CSR *csr, PrivateKey *privKey){
+    size_t csrInfo_size;
+    uint8_t *csrInfo_s = serialize_CSRInfo(csr->csrInfo, &csrInfo_size);
+    uint8_t *csrInfoHash = SHA512(csrInfo_s, csrInfo_size);
     
-    CSR *csr = (CSR *)malloc(sizeof(CSR));
+    mpz_t csrInfoHash_m;
+    mpz_init(csrInfoHash_m);
+    mpz_import(csrInfoHash_m, SHA512_DIGEST_LENGTH, 1, 1, 0, 0, csrInfoHash);
+    
+    csr->signatureAlgo = malloc(strlen("sha512WithRSAEncryption") + 1);
+    strcpy(csr->signatureAlgo, "sha512WithRSAEncryption");
+    
+    size_t signatureAlgo_size;
+    uint8_t *signatureAlgo_s = serialize_signAlgo(csr->signatureAlgo, &signatureAlgo_size);
 
-    csr->version = 0x00;  // v1
-    csr->subject = subject;
-    
-    csr->publicKey = extract_publicBytes(privateKey);
-    strcpy(csr->publicKey->algorithmIdentifier, "rsaEncryption"), csr->publicKey->algorithmIdentifier[14] = '\0';
-    csr->publicKey->keyBit = 2048;
-    
-    /* gmp_printf("modulus=%Zx\n", csr->publicKey->modulus);
-    gmp_printf("pub exp=%Zx\n", csr->publicKey->exponent);
-    gmp_printf("priv exp=%Zx\n", privateKey->priv_exp); */
-
-    size_t csr_size;
-    uint8_t *csr_s = serialize_CSR(*csr, &csr_size);
-    uint8_t *csrHash = SHA512(csr_s, csr_size);
-    
-    mpz_t csrHash_m;
-    mpz_init(csrHash_m);
-    mpz_import(csrHash_m, SHA512_DIGEST_LENGTH, 1, 1, 0, 0, csrHash);
-    
-    csr->signature = (Signature *)malloc(sizeof(Signature));
-    strcpy(csr->signature->algorithmIdentifier, "sha512WithRSAEncryption");
-    csr->signature->algorithmIdentifier[24] = '\0';
-    
-    RSA_private_encrypt(csr->signature->value, csrHash_m, privateKey->priv_exp, privateKey->modulus);
+    RSA_private_encrypt(csr->signature, csrInfoHash_m, privKey->d, privKey->n);
     
     size_t signature_size;
-    uint8_t *signature_s = serialize_signature(*csr->signature, &signature_size);
+    uint8_t *signature_s = serialize_signature(csr->signature, &signature_size);
     
-    size_t certRequest_size = csr_size + signature_size;
-    uint8_t *certRequest_s = (uint8_t *)malloc(certRequest_size);
-    if(certRequest_s){
-        int index = 0;
-
-        memcpy(certRequest_s + index, csr_s, csr_size);
-        index += csr_size;
-
-        memcpy(certRequest_s + index, signature_s, signature_size);
-        index += signature_size;
-
-        size_t buffer_size;
-        uint8_t *buffer = serialize_sequence(&buffer_size, certRequest_size, certRequest_s);
-        
-        if(write_cer_file(DATA_DIR "/csr.cer", certRequest_size, certRequest_s))
-            printf("CSR generated and saved as 'csr.cer'\n");
-        else
-            printf("CSR generation failed.\n");
-        
-        free(buffer);
-    }
-    else{
-        perror("Memory allocation failed");
+    size_t certRequest_size = csrInfo_size + signatureAlgo_size + signature_size;
+    uint8_t *certRequest_s = malloc(certRequest_size);
+    if(!certRequest_s){
+        perror("Error serializing CSR");
+        return NULL;
     }
     
-    free(csr_s);
-    free(csrHash);
+    int index = 0;
+    #define COLLECT_INDIVIDUAL(val_s, val_size)             \
+        memcpy(certRequest_s + index, val_s, val_size);     \
+        index += val_size;
+
+        COLLECT_INDIVIDUAL(csrInfo_s, csrInfo_size);
+        COLLECT_INDIVIDUAL(signatureAlgo_s, signatureAlgo_size);
+        COLLECT_INDIVIDUAL(signature_s, signature_size);
+
+    #undef COLLECT_INDIVIDUAL
+
+    uint8_t *buffer = serialize_sequence(output_size, certRequest_size, certRequest_s);
+
+    free(csrInfo_s);
+    free(csrInfoHash);
+    mpz_clear(csrInfoHash_m);
+    free(signatureAlgo_s);
     free(signature_s);
-    mpz_clear(csrHash_m);
+    free(certRequest_s);
+
+    return buffer;
+}
+
+CSR *createCSR(PrivateKey *privKey, DName *subject){
     
+    CSR *csr = init_csr();
+    if(!csr){ return NULL; }
+
+    get_csrInfo(csr->csrInfo, subject, privKey);
+    
+    size_t certRequest_size;
+    uint8_t *certRequest_s = serializeAndSignCSR(&certRequest_size, csr, privKey);
+
+    if(write_cer_file(DATA_DIR "/csr.cer", certRequest_size, certRequest_s))
+        printf("CSR generated and saved as 'csr.cer'\n");
+    else{
+        printf("CSR generation failed.\n");
+        free_csr(csr);
+        free(certRequest_s);
+        return NULL;
+    }
+    
+    free(certRequest_s);
     return csr;
 }
