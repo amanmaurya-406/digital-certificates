323f3a52804b30b58d666c445a013ce3bdb17e4c
amanmaurya-406 <amanmaurya8661@gmail.com>
2025-04-14 03:53:51 +0530

    changed function syntax

--- src/serialize.c
+++ src/serialize.c
@@ -12,46 +12,42 @@ uint8_t *serialize_serialNumber(mpz_t serialNumber, size_t *outputSize){
     return serialize_mpz(outputSize, serialNumber);
 }
 
-uint8_t *serialize_issuerInfo(Info caInfo, size_t *outputSize){
+uint8_t *serialize_issuerInfo(DName *issuer, size_t *outputSize){
     
     size_t country_size, org_size;
     
-    uint8_t *country_s = serialize_string(&country_size, MY_ASN1_PRINTABLESTRING, caInfo.country);
-    uint8_t *organization_s = serialize_string(&org_size, MY_ASN1_UTF8STRING, caInfo.organization);
+    uint8_t *country_s = serialize_string(&country_size, MY_ASN1_PRINTABLESTRING, issuer->country);
+    uint8_t *org_s = serialize_string(&org_size, MY_ASN1_UTF8STRING, issuer->org);
 
     size_t issuerInfo_size = country_size + org_size;
-    uint8_t *issuerInfo_s = (uint8_t *)malloc(issuerInfo_size);
+    uint8_t *issuerInfo_s = malloc(issuerInfo_size);
     if(!issuerInfo_s){
-        perror("Memory allocation failed\n");
+        perror("Error serializing Issuer Info");
         *outputSize = 0;
         return NULL;
     }
 
-    int index = 0;
-    memcpy(issuerInfo_s + index, country_s, country_size);
-    index += country_size;
-
-    memcpy(issuerInfo_s + index, organization_s, org_size);
-    index += org_size;
+    memcpy(issuerInfo_s, country_s, country_size);
+    memcpy(issuerInfo_s + country_size, org_s, org_size);
 
     uint8_t *buffer = serialize_sequence(outputSize, issuerInfo_size, issuerInfo_s);
     
     free(country_s);
-    free(organization_s);
+    free(org_s);
     free(issuerInfo_s);
     return buffer;
 }
 
-uint8_t *serialize_validity(Time time, size_t *outputSize){
+uint8_t *serialize_time(Time *t, size_t *outputSize){
 
     size_t year_size, month_size, day_size, hour_size, minute_size, second_size;
 
-    uint8_t *year_s = serialize_integer(&year_size, time.year);
-    uint8_t *month_s = serialize_integer(&month_size, time.month);
-    uint8_t *day_s = serialize_integer(&day_size, time.day);
-    uint8_t *hour_s = serialize_integer(&hour_size, time.hour);
-    uint8_t *minute_s = serialize_integer(&minute_size, time.minute);
-    uint8_t *second_s = serialize_integer(&second_size, time.second);
+    uint8_t *year_s = serialize_integer(&year_size, t->year);
+    uint8_t *month_s = serialize_integer(&month_size, t->month);
+    uint8_t *day_s = serialize_integer(&day_size, t->day);
+    uint8_t *hour_s = serialize_integer(&hour_size, t->hour);
+    uint8_t *minute_s = serialize_integer(&minute_size, t->minute);
+    uint8_t *second_s = serialize_integer(&second_size, t->second);
 
     size_t time_size = year_size + month_size + day_size + hour_size + minute_size + second_size;
     uint8_t *time_s = (uint8_t *)malloc(time_size);
@@ -88,20 +84,21 @@ uint8_t *serialize_validity(Time time, size_t *outputSize){
     return buffer;
 }
 
-uint8_t *serialize_subInfo(Info subInfo, size_t *outputSize){
+uint8_t *serialize_subInfo(DName *subject, size_t *outputSize){
 
-    size_t country_size, state_size, locality_size, organization_size, commonName_size;
+    size_t country_size, state_size, city_size, org_size, unit_size, name_size;
     
-    uint8_t *country_s = serialize_string(&country_size, MY_ASN1_PRINTABLESTRING, subInfo.country);
-    uint8_t *state_s = serialize_string(&state_size, MY_ASN1_UTF8STRING, subInfo.state);
-    uint8_t *locality_s = serialize_string(&locality_size, MY_ASN1_UTF8STRING, subInfo.locality);
-    uint8_t *organization_s = serialize_string(&organization_size, MY_ASN1_UTF8STRING, subInfo.organization);
-    uint8_t *commonName_s = serialize_string(&commonName_size, MY_ASN1_UTF8STRING, subInfo.common_name);
+    uint8_t *country_s = serialize_string(&country_size, MY_ASN1_PRINTABLESTRING, subject->country);
+    uint8_t *state_s = serialize_string(&state_size, MY_ASN1_UTF8STRING, subject->state);
+    uint8_t *city_s = serialize_string(&city_size, MY_ASN1_UTF8STRING, subject->city);
+    uint8_t *org_s = serialize_string(&org_size, MY_ASN1_UTF8STRING, subject->org);
+    uint8_t *unit_s = serialize_string(&unit_size, MY_ASN1_UTF8STRING, subject->org);
+    uint8_t *name_s = serialize_string(&name_size, MY_ASN1_UTF8STRING, subject->name);
 
-    size_t subInfo_size = country_size + state_size + locality_size + organization_size + commonName_size;
+    size_t subInfo_size = country_size + state_size + city_size + org_size + unit_size + name_size;
     uint8_t *subInfo_s = (uint8_t *)malloc(subInfo_size);
     if(!subInfo_s){
-        perror("Memory allocation failed");
+        perror("Error seriliazing DName");
         *outputSize = 0;
         return NULL;
     }
@@ -114,9 +111,10 @@ uint8_t *serialize_subInfo(Info subInfo, size_t *outputSize){
 
         COLLECT_INDIVIDUAL(country_s, country_size);
         COLLECT_INDIVIDUAL(state_s, state_size);
-        COLLECT_INDIVIDUAL(locality_s, locality_size);
-        COLLECT_INDIVIDUAL(organization_s, organization_size);
-        COLLECT_INDIVIDUAL(commonName_s, commonName_size);
+        COLLECT_INDIVIDUAL(city_s, city_size);
+        COLLECT_INDIVIDUAL(org_s, org_size);
+        COLLECT_INDIVIDUAL(unit_s, unit_size);
+        COLLECT_INDIVIDUAL(name_s, name_size);
 
     #undef COLLECT_INDIVIDUAL
 
@@ -124,90 +122,78 @@ uint8_t *serialize_subInfo(Info subInfo, size_t *outputSize){
     
     free(country_s);
     free(state_s);
-    free(locality_s);
-    free(organization_s);
-    free(commonName_s);
+    free(city_s);
+    free(org_s);
+    free(unit_s);
+    free(name_s);
     free(subInfo_s);
 
     return buffer;
 }
 
-uint8_t *serialize_subPubKeyInfo(PublicKey publicKey, size_t *outputSize){
+uint8_t *serialize_pKeyInfo(PKeyInfo *pKeyInfo, size_t *outputSize){
 
-    // 1. Serialize AlgorithmIdentifier
-    size_t pubKeyAlgoIdentifier_size;
-    uint8_t *pubKeyAlgoIdentifier_s = serialize_string(&pubKeyAlgoIdentifier_size, MY_ASN1_UTF8STRING, publicKey.algorithmIdentifier);
-
-    // 2. Serialize Public keyBit
-    size_t keyBit_size;;
-    uint8_t *keyBit_s = serialize_integer(&keyBit_size, publicKey.keyBit);
+    // Serialize algorithm
+    size_t pubKeyAlgo_size;
+    uint8_t *pubKeyAlgo_s = serialize_string(&pubKeyAlgo_size, MY_ASN1_UTF8STRING, pKeyInfo->pubKeyAlgo);
     
-    // 3. Serialize Public key modulus
+    // Serialize modulus
     size_t mod_size;
-    uint8_t *mod_s = serialize_mpz(&mod_size, publicKey.modulus);
+    uint8_t *mod_s = serialize_mpz(&mod_size, pKeyInfo->pubKey->n);
 
-    // 4. Serialize Public key exponent
+    // Serialize Public exponent
     size_t exp_size;
-    uint8_t *exp_s = serialize_mpz(&exp_size, publicKey.exponent);
+    uint8_t *exp_s = serialize_mpz(&exp_size, pKeyInfo->pubKey->e);
 
-    // 5. Serialize all key components in sequence
-    size_t publicKey_size = keyBit_size + exp_size + mod_size;
-    uint8_t *publicKey_s = malloc(publicKey_size);
-    if(!publicKey_s){
-        perror("Memory alloaction failed");
+    // wrapping key components in sequence
+    size_t pubKey_size = mod_size + exp_size;
+    uint8_t *pubKey_s = malloc(pubKey_size);
+    if(!pubKey_s){
+        perror("Error serializing public key");
         *outputSize = 0;
         return NULL;
     }
 
-    int index = 0;
+    memcpy(pubKey_s, mod_s, mod_size);
+    memcpy(pubKey_s + mod_size, exp_s, exp_size);
 
-    #define COLLECT_INDIVIDUAL(rop, individual, individual_size)          \
-        memcpy(rop + index, individual, individual_size);                 \
-        index += individual_size;
+    size_t xxx_size;
+    uint8_t *xxx_s = serialize_sequence(&xxx_size, pubKey_size, pubKey_s);
 
-        COLLECT_INDIVIDUAL(publicKey_s, keyBit_s, keyBit_size);
-        COLLECT_INDIVIDUAL(publicKey_s, mod_s, mod_size);
-        COLLECT_INDIVIDUAL(publicKey_s, exp_s, exp_size);
+    // wrapping algorithm and public key
+    size_t pubKeyInfo_size = pubKeyAlgo_size + xxx_size;
+    uint8_t *pubKeyInfo_s = (uint8_t *)malloc(pubKeyInfo_size);
+    if(!pubKeyInfo_s){
+        perror("Error serializing PKeyInfo");
+        *outputSize = 0;
+        return NULL;
+    }
 
-        size_t xxx_size;
-        uint8_t *xxx_s = serialize_sequence(&xxx_size, publicKey_size, publicKey_s);
+    memcpy(pubKeyInfo_s, pubKeyAlgo_s, pubKeyAlgo_size);
+    memcpy(pubKeyInfo_s + pubKeyAlgo_size, xxx_s, xxx_size);
 
-        // 6. sequence of AlgorithmIdentifier + Public key
-        size_t publicKeyInfo_size = pubKeyAlgoIdentifier_size + xxx_size;
-        uint8_t *publicKeyInfo_s = (uint8_t *)malloc(publicKeyInfo_size);
-        if(!publicKeyInfo_s){
-            perror("Memory alloaction failed");
-            *outputSize = 0;
-            return NULL;
-        }
 
-        index = 0;
-        COLLECT_INDIVIDUAL(publicKeyInfo_s, pubKeyAlgoIdentifier_s, pubKeyAlgoIdentifier_size);
-        COLLECT_INDIVIDUAL(publicKeyInfo_s, xxx_s, xxx_size);
+    uint8_t *buffer = serialize_sequence(outputSize, pubKeyInfo_size, pubKeyInfo_s);
 
-    #undef COLLECT_INDIVIDUAL
-
-    uint8_t *buffer = serialize_sequence(outputSize, publicKeyInfo_size, publicKeyInfo_s);
-
-    free(pubKeyAlgoIdentifier_s);
-    free(keyBit_s);
+    free(pubKeyAlgo_s);
     free(mod_s);
     free(exp_s);
-    free(publicKey_s);
+    free(pubKey_s);
     free(xxx_s);
-    free(publicKeyInfo_s);
+    free(pubKeyInfo_s);
+    
     return buffer;
 }
 
-uint8_t *serialize_CSR(CSR csr, size_t *outputSize){
+uint8_t *serialize_CSRInfo(CSRInfo *csrInfo, size_t *outputSize){
     
-    size_t ver_size, subInfo_size, PKeyInfo_size;
+    size_t ver_size, subInfo_size, pKeyInfo_size;
     
-    uint8_t *version_s = serialize_version(csr.version, &ver_size);
-    uint8_t *subInfo_s = serialize_subInfo(csr.subject, &subInfo_size);
-    uint8_t *PKeyInfo_s = serialize_subPubKeyInfo(*csr.publicKey, &PKeyInfo_size);
+    uint8_t *version_s = serialize_version(csrInfo->version, &ver_size);
+    uint8_t *subInfo_s = serialize_subInfo(csrInfo->subject, &subInfo_size);
+    uint8_t *pKeyInfo_s = serialize_pKeyInfo(csrInfo->pKeyInfo, &pKeyInfo_size);
     
-    size_t csr_size = ver_size + subInfo_size + PKeyInfo_size;
+    size_t csr_size = ver_size + subInfo_size + pKeyInfo_size;
     uint8_t *csr_s = (uint8_t *)malloc(csr_size);
     if(!csr_s){
         perror("Memory allocation failed");
@@ -223,7 +209,7 @@ uint8_t *serialize_CSR(CSR csr, size_t *outputSize){
 
         COLLECT_INDIVIDUAL(version_s, ver_size);
         COLLECT_INDIVIDUAL(subInfo_s, subInfo_size);
-        COLLECT_INDIVIDUAL(PKeyInfo_s, PKeyInfo_size);
+        COLLECT_INDIVIDUAL(pKeyInfo_s, pKeyInfo_size);
 
     #undef COLLECT_INDIVIDUAL
     
@@ -231,28 +217,28 @@ uint8_t *serialize_CSR(CSR csr, size_t *outputSize){
     
     free(version_s);
     free(subInfo_s);
-    free(PKeyInfo_s);
+    free(pKeyInfo_s);
     free(csr_s);
     
     return buffer;
 }
 
-uint8_t *serialize_certificate(Certificate *cert, size_t *outputSize){
+uint8_t *serialize_tbsCertificate(TBSCertificate *tbsCert, size_t *outputSize){
 
-    size_t ver_size, ser_size, subSignAlgo_size, issuerInfo_size, validFrom_size, validTo_size, subInfo_size, subPubKey_size;
+    size_t ver_size, ser_size, signatureAlgo_size, issuerInfo_size, validFrom_size, validTo_size, subInfo_size, pKeyInfo_size;
     
-    uint8_t *version_s = serialize_version(cert->version, &ver_size);
-    uint8_t *serialNumber_s = serialize_serialNumber(cert->serialNumber, &ser_size);
-    uint8_t *subSignAlgo_s = serialize_subSignAlgo(cert->subject_signAlgorithm, &subSignAlgo_size);
-    uint8_t *issuerInfo_s = serialize_issuerInfo(cert->issuer, &issuerInfo_size);
-    uint8_t *validFrom_s = serialize_validity(cert->validFrom, &validFrom_size);
-    uint8_t *validTo_s = serialize_validity(cert->validTo, &validTo_size);
-    uint8_t *subInfo_s = serialize_subInfo(cert->subject, &subInfo_size);
-    uint8_t *subPubKey_s = serialize_subPubKeyInfo(*cert->subject_pubKey, &subPubKey_size);
+    uint8_t *version_s = serialize_version(tbsCert->version, &ver_size);
+    uint8_t *serialNumber_s = serialize_serialNumber(tbsCert->serialNumber, &ser_size);
+    uint8_t *signatureAlgo_s = serialize_signAlgo(tbsCert->signatureAlgo, &signatureAlgo_size);
+    uint8_t *issuerInfo_s = serialize_issuerInfo(tbsCert->issuer, &issuerInfo_size);
+    uint8_t *validFrom_s = serialize_time(tbsCert->validity->validFrom, &validFrom_size);
+    uint8_t *validTo_s = serialize_time(tbsCert->validity->validTo, &validTo_size);
+    uint8_t *subInfo_s = serialize_subInfo(tbsCert->subject, &subInfo_size);
+    uint8_t *pKeyInfo_s = serialize_pKeyInfo(tbsCert->pKeyInfo, &pKeyInfo_size);
 
-    size_t cert_size = ver_size + ser_size + subSignAlgo_size + issuerInfo_size + validFrom_size + validTo_size + subInfo_size + subPubKey_size;
-    uint8_t *certificate_s = (uint8_t *)malloc(cert_size);
-    if(!certificate_s){
+    size_t tbsCert_size = ver_size + ser_size + signatureAlgo_size + issuerInfo_size + validFrom_size + validTo_size + subInfo_size + pKeyInfo_size;
+    uint8_t *tbsCert_s = (uint8_t *)malloc(tbsCert_size);
+    if(!tbsCert_s){
         perror("Memory allocation failed");
         *outputSize = 0;
         return NULL;
@@ -260,65 +246,42 @@ uint8_t *serialize_certificate(Certificate *cert, size_t *outputSize){
 
     int index = 0;
 
-    #define COLLECT_INDIVIDUAL(individual, individual_size)          \
-        memcpy(certificate_s + index, individual, individual_size);      \
-        index += individual_size;
+    #define COLLECT_INDIVIDUAL(val_s, val_size)          \
+        memcpy(tbsCert_s + index, val_s, val_size);      \
+        index += val_size;
 
         COLLECT_INDIVIDUAL(version_s, ver_size);
         COLLECT_INDIVIDUAL(serialNumber_s, ser_size);
-        COLLECT_INDIVIDUAL(subSignAlgo_s, subSignAlgo_size);
+        COLLECT_INDIVIDUAL(signatureAlgo_s, signatureAlgo_size);
         COLLECT_INDIVIDUAL(issuerInfo_s, issuerInfo_size);
         COLLECT_INDIVIDUAL(validFrom_s, validFrom_size);
         COLLECT_INDIVIDUAL(validTo_s, validTo_size);
         COLLECT_INDIVIDUAL(subInfo_s, subInfo_size);
-        COLLECT_INDIVIDUAL(subPubKey_s, subPubKey_size);
+        COLLECT_INDIVIDUAL(pKeyInfo_s, pKeyInfo_size);
 
     #undef COLLECT_INDIVIDUAL
 
-    uint8_t *buffer = serialize_sequence(outputSize, cert_size, certificate_s);
+    uint8_t *buffer = serialize_sequence(outputSize, tbsCert_size, tbsCert_s);
     
     free(version_s);
     free(serialNumber_s);
-    free(subSignAlgo_s);
+    free(signatureAlgo_s);
     free(issuerInfo_s);
     free(validFrom_s);
     free(validTo_s);
     free(subInfo_s);
-    free(subPubKey_s);
-    free(certificate_s);
+    free(pKeyInfo_s);
+    free(tbsCert_s);
 
     return buffer;
 }
 
-uint8_t *serialize_subSignAlgo(const char *algorithm, size_t *outputSize){
+uint8_t *serialize_signAlgo(const char *algorithm, size_t *outputSize){
     return serialize_string(outputSize, MY_ASN1_UTF8STRING, algorithm);
 }
 
-uint8_t *serialize_signature(Signature signature, size_t *outputSize){
-
-    size_t signAlgoIdentifier_size, signValue_size;
-    
-    uint8_t *signAlgoIdentifier_s = serialize_string(&signAlgoIdentifier_size, MY_ASN1_UTF8STRING, signature.algorithmIdentifier);
-    uint8_t *signValue_s = serialize_mpz(&signValue_size, signature.value);
-
-    *outputSize = signAlgoIdentifier_size + signValue_size;
-    uint8_t *buffer = (uint8_t *)malloc(*outputSize);
-    if(!buffer){
-        perror("Memory allocation failed");
-        *outputSize = 0;
-        return NULL;
-    }
-
-    int index = 0;
-    memcpy(buffer + index, signAlgoIdentifier_s, signAlgoIdentifier_size);
-    index += signAlgoIdentifier_size;
-
-    memcpy(buffer + index, signValue_s, signValue_size);
-    index += signValue_size;
-
-    free(signAlgoIdentifier_s);
-    free(signValue_s);
-    return buffer;
+uint8_t *serialize_signature(mpz_t signature, size_t *outputSize){
+    return serialize_mpz(outputSize, signature);
 }
 
 
